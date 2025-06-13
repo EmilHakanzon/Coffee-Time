5d7967ddd2f8b9af5c1fdbeabc854a35002df166
Emil Johansson <Emilaik@gmail.com>
2025-06-13 21:11:27 +0200

    feat: implement useCoffeeHome hook for managing coffee-related state and reminders

---
+++ src/hooks/useCoffeeHome.ts
@@ -0,0 +1,65 @@
+import { useFocusEffect } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useCallback, useEffect, useState } from "react";
+import type { CoffeeType, CoffeeLog } from "@/src/types/coffee";
+
+export function useCoffeeHome() {
+  const [selectedCoffeeType, setSelectedCoffeeType] =
+    useState<CoffeeType | null>(null);
+  const [lastCoffeeTime, setLastCoffeeTime] = useState<Date | null>(null);
+  const [nextReminderTime, setNextReminderTime] = useState<Date | null>(null);
+  const [coffeeLog, setCoffeeLog] = useState<CoffeeLog[]>([]);
+  const [reminderHours, setReminderHours] = useState(4);
+  const [userName, setUserName] = useState("");
+
+  // denna hook körs varje gång homepage blir aktiv och då
+  //hämtar den om på nytt.
+  useFocusEffect(
+    useCallback(() => {
+      (async () => {
+        const savedReminder = await AsyncStorage.getItem("reminder_hours");
+        const saveName = await AsyncStorage.getItem("profile_name");
+        const saveLog = await AsyncStorage.getItem("coffee_log");
+        if (saveName) setUserName(saveName);
+        if (savedReminder) setReminderHours(Number(savedReminder));
+        if (saveLog) {
+          const parsedLog = JSON.parse(saveLog);
+          setCoffeeLog(parsedLog);
+          // senaste kaffe tiden om man har loggats
+
+          if (parsedLog.length > 0) {
+            setLastCoffeeTime(new Date(parsedLog[0].timestamp));
+          } else {
+            setLastCoffeeTime(null);
+          }
+        } else {
+          setCoffeeLog([]);
+          setLastCoffeeTime(null);
+        }
+      })();
+    }, []),
+  );
+
+  //  Uppdaterar nästa påminnelse-tid varje gång senaste kaffe-tiden eller påminnelse-timmar ändras.
+  useEffect(() => {
+    if (lastCoffeeTime) {
+      const nextTime = new Date(
+        lastCoffeeTime.getTime() + reminderHours * 60 * 60 * 1000,
+      );
+      setNextReminderTime(nextTime);
+    }
+  }, [lastCoffeeTime, reminderHours]);
+
+  return {
+    selectedCoffeeType,
+    setSelectedCoffeeType,
+    lastCoffeeTime,
+    setLastCoffeeTime,
+    nextReminderTime,
+    coffeeLog,
+    setCoffeeLog,
+    reminderHours,
+    setReminderHours,
+    userName,
+  };
+}
